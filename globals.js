092ba787e367a35dae5c8ae45be88d7a98447b77
Stoyan Manev <61543561+stoyanmanev@users.noreply.github.com>
2022-11-13 00:41:53 +0200

    Socket initial commit
    Integrated socket connection. Update DB connected with socket

--- globals.js
+++ globals.js
@@ -1,7 +1,11 @@
 const FIREBASE_DB = 'https://cars-db-8a83e-default-rtdb.europe-west1.firebasedatabase.app';
 const CLIENT_URL = 'C:/VUE/Cars_App/cars_client/src/';
+const PORT = 8000;
+const SOCKETPORT = 8001;
 
 module.exports = {
  DB: FIREBASE_DB,
- CLIENT_URL: CLIENT_URL
+ CLIENT_URL: CLIENT_URL,
+ PORT,
+ SOCKETPORT
 }
